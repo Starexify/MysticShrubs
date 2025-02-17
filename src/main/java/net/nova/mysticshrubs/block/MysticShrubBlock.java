ed4a6c35d4f8a38dcd8a496d23f946fae44b0d34
Starexify <nightbear843@yahoo.com>
2025-02-17 19:31:04 +0100

    Fixed Mystic Shrub not swinging hand

--- src/main/java/net/nova/mysticshrubs/block/MysticShrubBlock.java
+++ src/main/java/net/nova/mysticshrubs/block/MysticShrubBlock.java
@@ -3,10 +3,8 @@ package net.nova.mysticshrubs.block;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
@@ -18,6 +16,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -39,7 +38,7 @@ public class MysticShrubBlock extends CropBlock {
     }
 
     @Override
-    protected boolean mayPlaceOn(BlockState pState, BlockGetter pLevel, BlockPos pPos) {
+    protected boolean mayPlaceOn(BlockState pState, BlockGetter level, BlockPos pos) {
         return pState.is(Blocks.GRASS_BLOCK) || pState.is(Blocks.DIRT);
     }
 
@@ -54,21 +53,21 @@ public class MysticShrubBlock extends CropBlock {
     }
 
     @Override
-    protected void randomTick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
-        if (!pLevel.isAreaLoaded(pPos, 1)) return;
-        int i = this.getAge(pState);
+    protected void randomTick(BlockState state, ServerLevel serverLevel, BlockPos pos, RandomSource random) {
+        int i = this.getAge(state);
         if (i < this.getMaxAge()) {
-            float f = getGrowthSpeed(pState, pLevel, pPos);
-            if (CommonHooks.canCropGrow(pLevel, pPos, pState, pRandom.nextInt((int) (25.0F / f) + 1) == 0)) {
-                pLevel.setBlock(pPos, this.getStateForAge(i + 1), 2);
-                CommonHooks.fireCropGrowPost(pLevel, pPos, pState);
+            float f = getGrowthSpeed(state, serverLevel, pos);
+            if (CommonHooks.canCropGrow(serverLevel, pos, state, random.nextInt((int) (25.0F / f) + 1) == 0)) {
+                serverLevel.setBlock(pos, getStateForAge(i + 1), 2);
+                CommonHooks.fireCropGrowPost(serverLevel, pos, state);
+                serverLevel.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(getStateForAge(i + 1)));
             }
         }
     }
 
     @Override
-    protected boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
-        return pLevel.canSeeSky(pPos) && super.canSurvive(pState, pLevel, pPos);
+    protected boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        return level.canSeeSky(pos) && super.canSurvive(state, level, pos);
     }
 
     @Override
@@ -77,36 +76,38 @@ public class MysticShrubBlock extends CropBlock {
     }
 
     @Override
-    public boolean isBonemealSuccess(Level pLevel, RandomSource pRandom, BlockPos pPos, BlockState pState) {
+    public boolean isBonemealSuccess(Level level, RandomSource random, BlockPos pos, BlockState state) {
         return false;
     }
 
     // On player interactions drops
     @Override
-    protected InteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
-        if (!level.isClientSide && state.getValue(AGE).equals(1)) {
-            // Drop item for age 1 and set the age to 0
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+        if (state.getValue(AGE).equals(1)) {
             level.destroyBlock(pos, true);
-            level.setBlock(pos, state.setValue(AGE, 0), 2);
-
+            BlockState smallMysticShrub = state.setValue(AGE, 0);
+            level.setBlock(pos, smallMysticShrub, 2);
+            level.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(player, smallMysticShrub));
             return InteractionResult.SUCCESS;
         }
-        return InteractionResult.FAIL;
+        return super.useWithoutItem(state, level, pos, player, hitResult);
     }
 
     @Override
     public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
         if (!level.isClientSide && state.getValue(AGE).equals(1)) {
             super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
-            return level.setBlock(pos, state.setValue(AGE, 0), 3);
+            BlockState smallMysticShrub = state.setValue(AGE, 0);
+            level.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(player, smallMysticShrub));
+            return level.setBlock(pos, smallMysticShrub, 3);
         }
         return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
     }
 
     // Slower destroy speed for age 0
     @Override
-    protected float getDestroyProgress(BlockState pState, Player pPlayer, BlockGetter pLevel, BlockPos pPos) {
-        if (pState.getValue(AGE) == 0) {
+    protected float getDestroyProgress(BlockState state, Player player, BlockGetter level, BlockPos pos) {
+        if (state.getValue(AGE) == 0) {
             return 0.05f;
         } else {
             return 1.0f;
@@ -120,7 +121,7 @@ public class MysticShrubBlock extends CropBlock {
     }
 
     @Override
-    protected VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
-        return SHAPE_BY_AGE[this.getAge(pState)];
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext pContext) {
+        return SHAPE_BY_AGE[this.getAge(state)];
     }
 }
