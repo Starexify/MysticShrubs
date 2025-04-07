36f4975f8e7ea8fecf5eaa0bb74f8e9156cb2429
ImVeryBad <nightbear843@yahoo.com>
2025-04-07 22:17:10 +0200

    1.0.0

--- src/main/java/net/nova/mystic_shrubs/data/DataGenerators.java
+++ src/main/java/net/nova/mystic_shrubs/data/DataGenerators.java
@@ -1,14 +1,18 @@
 package net.nova.mystic_shrubs.data;
 
-import net.minecraft.core.HolderLookup;
-import net.minecraft.data.PackOutput;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import net.nova.mystic_shrubs.data.loot.MSLootTableProvider;
 import net.nova.mystic_shrubs.data.tags.MSBiomeTagsProvider;
+import net.nova.mystic_shrubs.data.worldgen.MSBiomeModifiers;
+import net.nova.mystic_shrubs.data.worldgen.MSConfiguredFeatures;
+import net.nova.mystic_shrubs.data.worldgen.MSPlacedFeatures;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.Set;
 
 import static net.nova.mystic_shrubs.MysticShrubs.MODID;
 
@@ -17,14 +21,15 @@ public class DataGenerators {
 
     @SubscribeEvent
     public static void gatherData(GatherDataEvent.Client event) {
-        PackOutput output = event.getGenerator().getPackOutput();
-        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-
-        event.addProvider(new LangProvider(output));
-        event.addProvider(new MSModelProvider(output));
-        event.addProvider(new MSBiomeTagsProvider(output, lookupProvider));
-        event.addProvider(new MSLootTableProvider(output, lookupProvider));
-        event.addProvider(new DatapackProvider(output, lookupProvider));
-        event.addProvider(new SoundsProvider(output));
+        event.createProvider(LangProvider::new);
+        event.createProvider(MSModelProvider::new);
+        event.createProvider(MSBiomeTagsProvider::new);
+        event.createProvider(MSLootTableProvider::new);
+        event.createProvider(SoundsProvider::new);
+        event.createDatapackRegistryObjects(new RegistrySetBuilder()
+                        .add(Registries.CONFIGURED_FEATURE, MSConfiguredFeatures::bootstrap)
+                        .add(Registries.PLACED_FEATURE, MSPlacedFeatures::bootstrap)
+                        .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, MSBiomeModifiers::bootstrap),
+                Set.of(MODID));
     }
 }
--- src/main/java/net/nova/mystic_shrubs/events/GameEvents.java
+++ src/main/java/net/nova/mystic_shrubs/events/GameEvents.java
@@ -1,12 +1,12 @@
 package net.nova.mystic_shrubs.events;
 
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.TriState;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.util.TriState;
 import net.neoforged.neoforge.event.entity.player.ItemEntityPickupEvent;
 import net.nova.mystic_shrubs.init.MSItems;
 import net.nova.mystic_shrubs.init.Sounds;
